f9a90574969434a158dc6eab03c3ac4882940d50
Andrea Osmar <andrea88@live.se>
2021-05-11 16:23:40 +0200

    created headertitle & headerLeft for chatscreen

--- src/screens/ChatScreen.tsx
+++ src/screens/ChatScreen.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import { AntDesign, FontAwesome, Ionicons } from '@expo/vector-icons';
 
-export const ChatScreen = () => {
+interface Chat {
+	navigation: any;
+	route: any;
+}
+
+export const ChatScreen: React.FC<Chat> = ({ navigation, route }) => {
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			title: 'Chat',
+			headerTitleAlign: 'center',
+			headerBackTitleVisible: false, // remove?
+
+			headerTitle: () => (
+				<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+					<Avatar
+						rounded
+						source={{
+							uri: 'https://clinicforspecialchildren.org/wp-content/uploads/2016/08/avatar-placeholder.gif',
+						}}
+					/>
+					<Text style={{ color: '#fff', marginLeft: 10, fontWeight: '700' }}>
+						{route.params.chatName}
+					</Text>
+				</View>
+			),
+
+			headerLeft: () => (
+				<TouchableOpacity style={{ marginLeft: 10 }} onPress={navigation.goBack}>
+					<AntDesign name='arrowleft' size={24} color='#fff' />
+				</TouchableOpacity>
+			),
+		});
+	}, [navigation]);
+
 	return (
 		<View>
-			<Text>ChatScreen</Text>
+			<Text>{route.params.chatName}</Text>
 		</View>
 	);
 };

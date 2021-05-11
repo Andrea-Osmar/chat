3ecb9bb9604349075ec65ba0751af93f4553477a
Andrea Osmar <andrea88@live.se>
2021-05-11 13:33:01 +0200

    added navigation to HomeScreen

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { KeyboardAvoidingView, StyleSheet, View } from 'react-native';
 import { Button, Input, Image } from 'react-native-elements';
+import { auth } from '../../firebase';
 
 interface Login {
 	navigation: any;
@@ -10,6 +11,16 @@ interface Login {
 export const LoginScreen: React.FC<Login> = ({ navigation }) => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+
+	useEffect(() => {
+		// Does not add to the stack - replaces to Home screen
+		const unsubscribe = auth.onAuthStateChanged((authUser) => {
+			if (authUser) {
+				navigation.replace('Home');
+			}
+		});
+		return unsubscribe;
+	}, []);
 
 	const signIn = () => {};
 

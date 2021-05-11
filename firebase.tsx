c9d7908eaa2fe902217c02575c133dfebce174a0
Andrea Osmar <andrea88@live.se>
2021-05-11 10:28:19 +0200

    organized navigation, added firebase and npm packages

---
+++ firebase.tsx
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyAMEghnd-3YtUJDoypKBWW_sFcgCeXDYxw',
+	authDomain: 'chat-f7da7.firebaseapp.com',
+	projectId: 'chat-f7da7',
+	storageBucket: 'chat-f7da7.appspot.com',
+	messagingSenderId: '356612583321',
+	appId: '1:356612583321:web:7475d0632c06046add2d08',
+};

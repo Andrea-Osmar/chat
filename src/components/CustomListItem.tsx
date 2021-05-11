5e88f343c05ead79ae1a6992ab5bc61317178eb5
Andrea Osmar <andrea88@live.se>
2021-05-11 18:10:44 +0200

    displaying avatar and last message on chat room

--- src/components/CustomListItem.tsx
+++ src/components/CustomListItem.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements';
+import { db } from '../../firebase';
 
 interface Custom {
 	id: any;
@@ -13,18 +14,33 @@ export const CustomListItem: React.FC<Custom> = ({
 	chatName,
 	enterChat,
 }) => {
+	const [chatMessages, setChatMessages] = useState([]);
+
+	useEffect(() => {
+		const unsubscribe = db
+			.collection('chats')
+			.doc(id)
+			.collection('messages')
+			.orderBy('timestamp', 'desc')
+			.onSnapshot((snapshot) =>
+				setChatMessages(snapshot.docs.map((doc) => doc.data()))
+			);
+		return unsubscribe;
+	}, []);
 	return (
 		<ListItem key={id} onPress={() => enterChat(id, chatName)} bottomDivider>
 			<Avatar
 				rounded
 				source={{
-					uri: 'http://www.damodarcollege.edu.in/web/wp-content/uploads/2021/01/sample-photo1.jpg',
+					uri:
+						chatMessages?.[0]?.photoURL ||
+						'http://www.damodarcollege.edu.in/web/wp-content/uploads/2021/01/sample-photo1.jpg',
 				}}
 			/>
 			<ListItem.Content>
 				<ListItem.Title style={{ fontWeight: 'bold' }}>{chatName}</ListItem.Title>
 				<ListItem.Subtitle numberOfLines={1} ellipsizeMode={'tail'}>
-					Subtitle Text
+					{chatMessages?.[0]?.dislayName}: {chatMessages?.[0]?.message}
 				</ListItem.Subtitle>
 			</ListItem.Content>
 		</ListItem>

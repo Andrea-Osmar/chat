01fbe2fe65d3b8076c1d7bcf9adab08796f65a90
Andrea Osmar <andrea88@live.se>
2021-05-11 12:40:19 +0200

    setup firebase

--- firebase.tsx
+++ firebase.tsx
@@ -1,3 +1,7 @@
+import * as firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
+
 const firebaseConfig = {
 	apiKey: 'AIzaSyAMEghnd-3YtUJDoypKBWW_sFcgCeXDYxw',
 	authDomain: 'chat-f7da7.firebaseapp.com',
@@ -6,3 +10,14 @@ const firebaseConfig = {
 	messagingSenderId: '356612583321',
 	appId: '1:356612583321:web:7475d0632c06046add2d08',
 };
+
+let app;
+
+if (firebase.apps.length === 0) {
+	app = firebase.initializeApp(firebaseConfig);
+} else {
+	app = firebase.app();
+}
+
+export const db = app.firestore();
+export const auth = firebase.auth();
